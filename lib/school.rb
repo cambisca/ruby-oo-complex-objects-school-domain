4491edc1417459cf63fa81b154198e4bbf0f52f5
Cameron Bisca <cjbisca@gmail.com>
2020-11-17 20:40:58 -0500

    commit tree 0598233c98c1100b8d5ebfbbe4809f723b6412a7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School  
+    def initialize(name)
+        @name = name 
+    end 
+
+    def roster 
+        @students = {}
+    end 
+
+    def add_student(name, grade) 
+        @students[grade] = []
+        @students[grade] << name 
+    end 
+end 
